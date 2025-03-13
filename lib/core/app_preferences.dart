9f4d32619c1b8bea63572c970a1c6d9dceb97cd8
Mekonnen <mekonnen@davidwilliston.com>
2025-03-13 09:46:27 +0300

    Enhance AppPreferences with additional methods for theme mode and language management

--- lib/core/app_preferences.dart
+++ lib/core/app_preferences.dart
@@ -4,7 +4,11 @@ import 'package:real_social_app/core/enums.dart';
 import 'package:real_social_app/core/theme/theme_strings.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+/// A class that handles application preferences such as theme mode and language settings.
+///
 class AppPreferences {
+  /// Sets the theme mode in shared preferences.
+  ///
   Future<void> setThemeMode(ThemeMode themeMode) async {
     final prefs = await SharedPreferences.getInstance();
     String themeString;
@@ -21,6 +25,8 @@ class AppPreferences {
     await prefs.setString(AppConstants.themeKey, themeString);
   }
 
+  /// Gets the theme mode from shared preferences.
+  ///
   ThemeMode getThemeMode(SharedPreferences prefs) {
     final themeString = prefs.getString(AppConstants.themeKey);
     if (themeString == ThemeStrings.light) return ThemeMode.light;
@@ -28,11 +34,15 @@ class AppPreferences {
     return ThemeMode.system;
   }
 
+  /// Sets the language in shared preferences.
+  ///
   Future<void> setLanguage(String language) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString(AppConstants.languageKey, language);
   }
 
+  /// Gets the language from shared preferences.
+  ///
   Lango getLanguage(SharedPreferences prefs) {
     final languageString = prefs.getString(AppConstants.languageKey);
     if (languageString == null) {
