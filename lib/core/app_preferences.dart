53899e471cab48aa2d3fa1e7ddb2f237a9a5490b
Mekonnen <mekonnen@davidwilliston.com>
2025-03-13 09:44:00 +0300

    Add AppConstants and ThemeStrings for centralized key management and theme representation

--- lib/core/app_preferences.dart
+++ lib/core/app_preferences.dart
@@ -1,40 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:real_social_app/core/app_constants.dart';
 import 'package:real_social_app/core/enums.dart';
+import 'package:real_social_app/core/theme/theme_strings.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AppPreferences {
-  static const String _themeKey = 'theme_mode';
-
-  ThemeMode getThemeMode(SharedPreferences prefs) {
-    final themeString = prefs.getString(_themeKey);
-    if (themeString == 'light') return ThemeMode.light;
-    if (themeString == 'dark') return ThemeMode.dark;
-    return ThemeMode.system;
-  }
-
   Future<void> setThemeMode(ThemeMode themeMode) async {
     final prefs = await SharedPreferences.getInstance();
     String themeString;
     switch (themeMode) {
       case ThemeMode.light:
-        themeString = 'light';
+        themeString = ThemeStrings.light;
         break;
       case ThemeMode.dark:
-        themeString = 'dark';
+        themeString = ThemeStrings.dark;
         break;
       default:
-        themeString = 'system';
+        themeString = ThemeStrings.system;
     }
-    await prefs.setString(_themeKey, themeString);
+    await prefs.setString(AppConstants.themeKey, themeString);
+  }
+
+  ThemeMode getThemeMode(SharedPreferences prefs) {
+    final themeString = prefs.getString(AppConstants.themeKey);
+    if (themeString == ThemeStrings.light) return ThemeMode.light;
+    if (themeString == ThemeStrings.dark) return ThemeMode.dark;
+    return ThemeMode.system;
   }
 
   Future<void> setLanguage(String language) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setString('language', language);
+    await prefs.setString(AppConstants.languageKey, language);
   }
 
   Lango getLanguage(SharedPreferences prefs) {
-    final languageString = prefs.getString('language');
+    final languageString = prefs.getString(AppConstants.languageKey);
     if (languageString == null) {
       return Lango.en;
     } else {
