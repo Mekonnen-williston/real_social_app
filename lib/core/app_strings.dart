8ea7135ef4046534951f19d9b0b4e49068086b5f
Mekonnen <mekonnen@davidwilliston.com>
2025-03-13 06:32:59 +0300

    Remove redundant comment in AppStrings class documentation

--- lib/core/app_strings.dart
+++ lib/core/app_strings.dart
@@ -1,5 +1,4 @@
 /// This file contains all the string constants used in the app.
-/// It includes titles, error messages, and other text used throughout the app.
 ///
 class AppStrings {
   /// This class contains the string constants used in the app.
